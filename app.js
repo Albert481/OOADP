f7aa672e9df85df7b09f73cfddaea93fdae7b76e
Albert <albertyu481@gmail.com>
2018-06-02 22:00:44 +0800

    Base

---
+++ app.js
@@ -0,0 +1,29 @@
+
+// Import basic modules
+var express = require('express');
+var path = require('path');
+var socket = require('socket.io');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// App setup
+var app = express();
+var io = socket();
+var serverPort = 3000;
+var httpServer = require('http').Server(app);
+
+// Import controllers
+
+
+// Setup routes
+
+
+
+// Static files
+
+
+app.set('port', serverPort);
+
+var server = httpServer.listen(app.get('port'), function (){
+    console.log('http server running on port' + server.address().port);
+});

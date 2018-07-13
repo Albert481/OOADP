ed5282a80371f554ece74801ce892bb46fb040af
magiicarp <171702L@mymail.nyp.edu.sg>
2018-07-13 21:24:04 +0800

    commented out bc code wasn't working uwu;;;

--- server/controllers/chatmessage.js
+++ server/controllers/chatmessage.js
@@ -1,41 +1,41 @@
-var ChatMsg = require('../models/chatMsg');
-var Conversation = require('../models/conversation')
-var myDatabase = require('./database');
-var sequelize = myDatabase.sequelize;
+// var ChatMsg = require('../models/chatMsg');
+// var Conversation = require('../models/conversation')
+// var myDatabase = require('./database');
+// var sequelize = myDatabase.sequelize;
+
+// // exports.receive = function(req, res) {
+// //     Conversation.findAll().then((conversations) => {
+// //         ChatMsg.findAll().then((chatMessages) => {
+// //             User.findAll().then((users) => {
+// //                 res.render('chatMsg', {
+// //                     title: 'myShoppe',
+// //                     conversations: conversations,
+// //                     chatmessages: chatMessages,
+// //                     urlPath: req.protocol + "://" + req.get("host") + req.path
+// //                 })
+// //             });
+// //         });
+// //     });
+// // };
 
 // exports.receive = function(req, res) {
+//     if (req.params.con_id == null){
+//         con_id = 0
+//     } else {
+//         con_id = req.params.con_id
+//     }
 //     Conversation.findAll().then((conversations) => {
-//         ChatMsg.findAll().then((chatMessages) => {
-//             User.findAll().then((users) => {
-//                 res.render('chatMsg', {
-//                     title: 'myShoppe',
-//                     conversations: conversations,
-//                     chatmessages: chatMessages,
-//                     urlPath: req.protocol + "://" + req.get("host") + req.path
-//                 })
+//         sequelize.query('select msg.conversation_id, msg.senderid, msg.recipientid, msg.message, msg.timestamp from ChatMsgs msg where msg.conversation_id =' + con_id, { model: ChatMsg} ).then((chats) => {
+//             res.render('chatMsg', {
+//                 title: 'myShoppe',
+//                 chatmessages: chats,
+//                 conversations: conversations,
+//                 urlPath: req.protocol + "://" + req.get("host") + req.path
+//             });
+//         }).catch((err) => {
+//             return res.status(400).send({
+//                 message: err
 //             });
 //         });
-//     });
+//     })
 // };
-
-exports.receive = function(req, res) {
-    if (req.params.con_id == null){
-        con_id = 0
-    } else {
-        con_id = req.params.con_id
-    }
-    Conversation.findAll().then((conversations) => {
-        sequelize.query('select msg.conversation_id, msg.senderid, msg.recipientid, msg.message, msg.timestamp from ChatMsgs msg where msg.conversation_id =' + con_id, { model: ChatMsg} ).then((chats) => {
-            res.render('chatMsg', {
-                title: 'myShoppe',
-                chatmessages: chats,
-                conversations: conversations,
-                urlPath: req.protocol + "://" + req.get("host") + req.path
-            });
-        }).catch((err) => {
-            return res.status(400).send({
-                message: err
-            });
-        });
-    })
-};
--- server/models/chatMsg.js
+++ server/models/chatMsg.js
@@ -1,51 +1,51 @@
-// models/chatMsg.js
-var myDatabase = require('../controllers/database');
-var sequelize = myDatabase.sequelize;
-var Sequelize = myDatabase.Sequelize;
+// // models/chatMsg.js
+// var myDatabase = require('../controllers/database');
+// var sequelize = myDatabase.sequelize;
+// var Sequelize = myDatabase.Sequelize;
 
-const ChatMsg = sequelize.define('ChatMsg', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    conversation_id: {
-        type: Sequelize.INTEGER,
-        // references: {
-        //     model: 'conversation',
-        //     key: 'con_id'
-        // }
-    },
-    senderid: {
-        type: Sequelize.INTEGER
-    },
-    recipientid: {
-        type: Sequelize.INTEGER   
-    },
-    message: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        defaultValue: '',
-        trim: true
-    },
-    timestamp: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    }
-});
+// const ChatMsg = sequelize.define('ChatMsg', {
+//     id: {
+//         type: Sequelize.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true
+//     },
+//     conversation_id: {
+//         type: Sequelize.INTEGER,
+//         // references: {
+//         //     model: 'conversation',
+//         //     key: 'con_id'
+//         // }
+//     },
+//     senderid: {
+//         type: Sequelize.INTEGER
+//     },
+//     recipientid: {
+//         type: Sequelize.INTEGER   
+//     },
+//     message: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//         defaultValue: '',
+//         trim: true
+//     },
+//     timestamp: {
+//         type: Sequelize.STRING,
+//         allowNull: true,
+//     }
+// });
 
-// force: true will drop the table if it already exists
-ChatMsg.sync({ force: false, logging: console.log}).then(() => {
-    // Table created
-    console.log("ChatMsg table synced");
-    ChatMsg.upsert({
-        id: '1',
-        conversation_id: '1',
-        senderid: '1',
-        recipientid: '2',
-        message: 'Hello World',
-        timestamp: '00:00 AM'
-    })
-});
+// // force: true will drop the table if it already exists
+// ChatMsg.sync({ force: false, logging: console.log}).then(() => {
+//     // Table created
+//     console.log("ChatMsg table synced");
+//     ChatMsg.upsert({
+//         id: '1',
+//         conversation_id: '1',
+//         senderid: '1',
+//         recipientid: '2',
+//         message: 'Hello World',
+//         timestamp: '00:00 AM'
+//     })
+// });
 
-module.exports = sequelize.model('ChatMsg', ChatMsg);
+// module.exports = sequelize.model('ChatMsg', ChatMsg);
